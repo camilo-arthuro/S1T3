2939c6d30991d3df81e382be0ebd5a044feeb260
Carlos Galan <cgalan23@ilg.cat>
2024-05-07 11:31:35 +0200

    07/05/2024 - 11:31

--- src/main/java/Exercise3.java
+++ src/main/java/Exercise3.java
@@ -7,14 +7,6 @@ import java.util.Set;
 
 public class Exercise3 {
     public static void runProgram() {
-        /*Crea una clase llamada Classes.Month con un atributo "name" (que almacenará el nombre del mes del año).
-        Añade 11 objetos Classes.Month (cada uno con su atributo diferente) en un ArrayList, a excepción del objeto con
-        atributo "Agost". Después, efectúa la inserción en el sitio que corresponde a este mes y demuestra que
-        ArrayList mantiene el orden correcto.
-
-        Convierte ArrayList del ejercicio anterior en un HashSet y asegúrate de que no permite duplicados.
-        Recorre la lista con un for y con un iterador.*/
-
         // Creating the ArrayList
         ArrayList<Month> monthList = new ArrayList<Month>();
 
